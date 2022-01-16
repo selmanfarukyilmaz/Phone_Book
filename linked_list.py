48b2593d3eae844c8a0bf2c72aaee6dba22b712f
Mustafa Gok <sd.mustafagok@gmail.com>
2022-01-16 20:55:23 +0000

    Refactor add and show methods

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Node:
 
     def __init__(self, data):
@@ -8,4 +11,26 @@ class Node:
 class LinkedList:
 
     def __init__(self):
-        self.head = None
+        self.head: Optional[Node] = None
+
+    def show(self):
+        """
+        Print nodes of the linked list to the screen
+        """
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+    def add(self, new_node: Node):
+        # for empty
+        if not self.head:
+            self.head = new_node
+            return
+
+        # find last node
+        last = self.head
+        while last.next:
+            last = last.next
+        # insert
+        last.next = new_node
--- main.py
+++ main.py
@@ -1,44 +1,25 @@
 from linked_list import LinkedList, Node
 from record import Record
 
+
+def add_record(linked_list: LinkedList, name: str, number: str):
+    new_record = Record(name=name, number=number)
+    new_node = Node(data=new_record)
+    linked_list.add(new_node)
+
+
 if __name__ == '__main__':
     r1 = Record(name="selman", number="541")
     r2 = Record(name="mustafa", number="542")
-    r3 = Record(name="fatih", number="543")
     r1_node = Node(data=r1)
     r2_node = Node(data=r2)
-    r3_node = Node(data=r3)
     phone_book = LinkedList()
     phone_book.head = r1_node
     r1_node.next = r2_node
-    r2_node.next = r3_node
 
-
-    def printList():
-        temp = phone_book.head
-        while temp:
-            print(f"name: {temp.data.name} ---> number: {temp.data.number}")
-            temp = temp.next
-
-
-    def add(name, number):
-        phone_book_dic = {}
-        temp = phone_book.head
-        while temp:
-            phone_book_dic[temp.data.name] = temp.data.number
-            temp = temp.next
-
-        if name in phone_book_dic.keys():
-            print(f"The name {name} is already in the phone book.\nPlease try again.")
-            return
-        elif number in phone_book_dic.values():
-            print(f"The number {number} is already in the phone book.\nPlease try again.")
-            return
-
-        r_new = Record(name, number)
-        new_node = Node(data=r_new)
-        new_node.next = phone_book.head
-        phone_book.head = new_node
+    add_record(linked_list=phone_book, name="fatih", number="543")
+    add_record(linked_list=phone_book, name="kadir", number="544")
+    phone_book.show()
 
 
     def remove(name):
