3cd0febe005b4a71e78ad57ff65fa752e07e2aa0
Mustafa Gok <sd.mustafagok@gmail.com>
2022-01-16 19:58:58 +0000

    Override str, repr and eq methods of Record

--- record.py
+++ record.py
@@ -4,3 +4,13 @@ class Record:
         self.name = name
         self.number = number
 
+    def __str__(self) -> str:
+        return f"{self.name}: {self.number}"
+
+    def __repr__(self) -> str:
+        return f"Record({self.name}, {self.number})"
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, Record):
+            return self.name == o.name and self.number == o.number
+        return super().__eq__(o)
