5873590c34b06b0c899edb2b23d73065366ad7ab
selmanfarukyilmaz <96119894+selmanfarukyilmaz@users.noreply.github.com>
2022-01-25 18:55:09 +0300

    Added Save, Upload and Reset functions.
    Code readability has been improved.
    Removed unnecessary codes

--- phone_book_operations.py
+++ phone_book_operations.py
@@ -1,7 +1,10 @@
+import os.path
+import time
+
+from termcolor import colored
+
 from linked_list import LinkedList, Node
 from record import Record
-from termcolor import colored
-import os.path
 
 
 def add_record(linked_list: LinkedList):
@@ -10,13 +13,11 @@ def add_record(linked_list: LinkedList):
     The name and number information of the person to be added to the phone book is obtained from the user.
 
     """
-    print("Please enter the name of the person you want to add to the phone book: ")
-    name = str(input()).title()
+    name = str(input("Please enter the name of the person you want to add to the phone book: "))
     if not name:
         print("You did not enter a valid name")
         return
-    print("Please enter the number of the person you want to add to the phone book: ")
-    number = str(input())
+    number = str(input("Please enter the number of the person you want to add to the phone book: "))
     if not number:
         print("You did not enter a valid number")
         return
@@ -34,8 +35,7 @@ def remove_record(linked_list: LinkedList):
     It asks the user for the name of the user registered in the phone book and then deletes it.
 
     """
-    print("Please enter the name of the person you want to delete from the phone book: ")
-    name = str(input()).title()
+    name = str(input("Please enter the name of the person you want to delete from the phone book: "))
 
     if linked_list.head.data.name == name:
         linked_list.head = linked_list.head.next
@@ -60,8 +60,7 @@ def search_prefix(linked_list: LinkedList):
     The results starting with the input entered by the user are printed on the screen.
 
     """
-    print("Enter the name you want to search in the phone book: ")
-    prefix = str(input()).title()
+    prefix = str(input("Enter the name you want to search in the phone book: "))
     temp = linked_list.head
     count = 0
     while temp:
@@ -78,8 +77,7 @@ def update(linked_list: LinkedList):
     Updates the name or number of a contact in the phone book.
     """
     temp = linked_list.head
-    print("Please enter the registered name whose information you want to update: ")
-    input_name = str(input()).title()
+    input_name = str(input("Please enter the registered name whose information you want to update: "))
     check = False
     while temp:
         if temp.data.name == input_name:
@@ -91,98 +89,106 @@ def update(linked_list: LinkedList):
         print(f"{input_name} name not found in phone book")
         return
 
-    print(f"Enter the new name of {input_name} you want to update: (Press enter for no change)")
-    input_new_name = str(input()).title()
+    input_new_name = str(input(f"Enter the new name of {input_name} you want to update: (Press enter for no change)"))
 
-    print(f"Enter the new number of {input_name} you want to update: (Press enter for no change)")
-    input_new_number = str(input())
+    input_new_number = str(
+        input(f"Enter the new number of {input_name} you want to update: (Press enter for no change)"))
 
-    if input_new_name:
-        temp.data.name = input_new_name
-    if input_new_number:
-        temp.data.number = input_new_number
+    temp.data.name = input_new_name or temp.data.name
+    temp.data.number = input_new_number or temp.data.number
     print(f"{temp.data.name}: {temp.data.number}")
 
-    if temp is None:
-        return
+
+def reset(linked_list: LinkedList):
+    """
+    It deletes all data and creates a new phone book.
+    """
+    variable = input("This operation will delete all record information in the phone book. Press \"1\" to perform the "
+                     "reset.\nPress any key to cancel")
+    if variable == 1:
+        linked_list.head = None
+
+
+def load_records(linked_list: LinkedList):
+    global delimiter_line
+    global delimiter_word
+    global txt_name
+    txt_name = "phonebook.txt"
+    delimiter_line = "\n"
+    delimiter_word = ","
+
+    if os.path.isfile(txt_name):
+        txt_name = txt_name
+        with open(file=txt_name, mode="r") as file:
+            records = file.read()
+            records = records.split(delimiter_line)
+            for r in records[:-1]:
+                name, number = r.split(delimiter_word)
+                new_record = Record(name=name, number=number)
+                new_node = Node(data=new_record)
+                linked_list.add(new_node)
+
+
+def save(linked_list: LinkedList):
+    temp = linked_list.head
+    with open(file=txt_name, mode="w") as file:
+        while temp:
+            file.write(f"{temp.data.name}{delimiter_word}{temp.data.number}{delimiter_line}")
+            temp = temp.next
+
+
+def menu():
+    print(colored("↓ Select the Action you want to use ↓", "green", attrs=['bold']))
+    print(colored("1- View contacts in the phone book.\n"
+                  "2- Add a new contact to the phone book.\n"
+                  "3- Delete a contact in the phone book.\n"
+                  "4- Update the information of a contact in the phone book.\n"
+                  "5- Searching the phone book by name.\n"
+                  "6- Reset phone book.\n"
+                  "7- Exit", attrs=['bold']))
+
+
+def help_me():
+    print("Please press 1,2,3,4,5 or 6 according to the operation you want to do.")
+    time.sleep(2)
+    input("Press any key to continue")
 
 
 def interface(linked_list: LinkedList):
-    if os.path.isfile("phonebook.txt"):
-        text_file = open("phonebook.txt", "r")
-        data = text_file.read()
-        text_file.close()
-        data = data.split("*+-")
-
-        name_temp = []
-        number_temp = []
-        for i, datas in enumerate(data):
-            if i % 2 == 0:
-                name_temp.append(datas)
-            else:
-                number_temp.append(datas)
-
-        for name, number in zip(name_temp, number_temp):
-            new_record = Record(name=name, number=number)
-            new_node = Node(data=new_record)
-            linked_list.add(new_node)
-
     print(colored(">>>>>>>>Welcome to Phone Book<<<<<<<<", "yellow", attrs=['bold']))
-    while True:
-        print(colored("↓ Select the Action you want to use ↓", "green", attrs=['bold']))
-        print(colored("1- View contacts in the phone book.\n"
-                      "2- Add a new contact to the phone book.\n"
-                      "3- Delete a contact in the phone book.\n"
-                      "4- Update the information of a contact in the phone book.\n"
-                      "5- Searching the phone book by name.\n"
-                      "6- Exit", attrs=['bold']))
 
+    load_records(linked_list)
+    action = 0
+    while action != "7":
+
+        menu()
         action = str(input())
 
         if action == "1":
             linked_list.show()
-            print("Press any key to continue")
-            input()
-            continue
+            input("Press any key to continue")
 
-        if action == "2":
+        elif action == "2":
             add_record(linked_list)
-            print("Press any key to continue")
-            input()
-            continue
+            input("Press any key to continue")
 
-        if action == "3":
+        elif action == "3":
             remove_record(linked_list)
-            print("Press any key to continue")
-            input()
-            continue
+            input("Press any key to continue")
 
-        if action == "4":
+        elif action == "4":
             update(linked_list)
-            print("Press any key to continue")
-            input()
-            continue
+            input("Press any key to continue")
 
-        if action == "5":
+        elif action == "5":
             search_prefix(linked_list)
-            print("Press any key to continue")
-            input()
-            continue
+            input("Press any key to continue")
 
-        if action == "6":
-            temp = linked_list.head
-            f = open("phonebook.txt", "w")
-            while temp:
-                name_str = str(temp.data.name + "*+-")
-                f.write(name_str)
-                number_str = str(temp.data.number + "*+-")
-                f.write(number_str)
-                temp = temp.next
+        elif action == "6":
+            reset(linked_list)
 
-            f.close()
-            break
+        elif action == "7":
+            save(linked_list)
 
         else:
-            print("Please press 1,2,3,4,5 or 6 according to the operation you want to do.")
-            print("Press any key to continue")
-            input()
+            help_me()
