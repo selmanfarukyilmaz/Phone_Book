ade7dea57e4f0b3ab79888c1649d8d5b925b0a20
selmanfarukyilmaz <96119894+selmanfarukyilmaz@users.noreply.github.com>
2022-01-28 05:39:06 +0300

    Created class PhoneRecord, class Person

--- record.py
+++ record.py
@@ -1,16 +1,37 @@
-class Record:
+from linked_list import DoublyLinkedList
 
-    def __init__(self, name, number):
+
+class Person:
+
+    def __init__(self, name: str, numbers: DoublyLinkedList):
         self.name = name
+        self.numbers = numbers
+
+    def __str__(self) -> str:
+        return f"{self.name} --> {self.numbers}"
+
+    def __repr__(self) -> str:
+        return f"Record({self.name}, {self.numbers})"
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, Person):
+            return self.name == o.name and self.numbers == o.numbers
+        return super().__eq__(o)
+
+
+class PhoneRecord:
+
+    def __init__(self, number_type: str, number: str):
+        self.number_type = number_type
         self.number = number
 
     def __str__(self) -> str:
-        return f"{self.name}: {self.number}"
+        return f"{self.number_type}: {self.number}"
 
     def __repr__(self) -> str:
-        return f"Record({self.name}, {self.number})"
+        return f"Record({self.number_type}, {self.number})"
 
     def __eq__(self, o: object) -> bool:
-        if isinstance(o, Record):
-            return self.name == o.name and self.number == o.number
+        if isinstance(o, PhoneRecord):
+            return self.number_type == o.number_type and self.number == o.number
         return super().__eq__(o)
