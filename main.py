5b1ca6c7ca5b0393ff7d935f12400cc62c1d5a8a
selmanfarukyilmaz <96119894+selmanfarukyilmaz@users.noreply.github.com>
2022-02-04 23:07:35 +0300

    mein update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from linked_list import LinkedList
-
+from linked_list import DoublyLinkedList
 from phone_book_operations import interface
 
 if __name__ == '__main__':
-    phone_book = LinkedList()
+    phone_book = DoublyLinkedList()
 
     interface(phone_book)
